54ee17df38f5bf91a8fb74ea0534131b03e50bcb
Israel Vargas Merljak <imerljak@gmail.com>
2020-12-09 01:07:29 -0300

    migrate App.tsx to arrow func notation

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import './App.css';
 import { BasketContextProvider } from './app/contexts/BasketContext';
 import Shop from './app/pages/Shop';
 
-function App() {
+const App: React.FC = () => {
   return (
     <BasketContextProvider>
       <Shop />
     </BasketContextProvider>
   );
-}
+};
 
 export default App;
