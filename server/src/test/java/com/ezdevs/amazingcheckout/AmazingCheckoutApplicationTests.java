fa083b0bd7fb0f091819b26fa9fa3fdd93dfe0ed
Israel Merljak <imerljak@gmail.com>
2020-12-10 00:16:55 -0300

    moving frontend project to /client and creating server project

---
+++ server/src/test/java/com/ezdevs/amazingcheckout/AmazingCheckoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ezdevs.amazingcheckout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazingCheckoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
