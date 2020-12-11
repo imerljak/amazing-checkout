b5fa3bd62cc274082581e13a3884edaa45a797a7
Israel Merljak <imerljak@gmail.com>
2020-12-10 23:05:34 -0300

    changing colors to grayscale

--- client/src/app/components/Card/styles.ts
+++ client/src/app/components/Card/styles.ts
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 export const Card = styled.div`
   display: flex;
   flex-flow: column;
-  background-color: rgba(200, 200, 200, 0.3);
-  border-radius: 0.5rem;
+  background-color: #eee;
+  border-radius: 0.3rem;
   box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.5);
   padding: 0.5rem 1rem;
 `;
